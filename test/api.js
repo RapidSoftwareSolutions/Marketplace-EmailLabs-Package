a3e859fedac53ffbb14045587cf6c266c8f392b7
Dima Shirokov <dima.shirokoff@rapidapi.com>
2016-11-25 10:20:40 +0200

    require

--- test/api.js
+++
@@ -1,9 +0,0 @@
-const app     = require('../index');
-const assert  = require('chai').assert;
-const request = require('supertest-as-promised');
-
-describe('LinkedIn API', () => {
-    it('...', function() {
-      console.log('Todo: session trouble');
-    });    
-});
